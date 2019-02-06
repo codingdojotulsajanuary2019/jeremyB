ef89e6fa24d21a116ff931bc7370005e8d2d2982
JBenedik <jeremybenedik@gmail.com>
2019-02-06 12:29:41 -0600

    python group project

---
+++ python/python/dataStructures/sl_main.py
@@ -0,0 +1,5 @@
+from sl_list import SList
+from sl_node import Node
+
+my_list = SList()
+my_list.add_to_front(7).add_to_back(4).add_to_back(3).add_to_back(9).add_to_back(3).add_to_back(1).insert_at(5,6).print_values()
