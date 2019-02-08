75b15652b0c69b9a55e90eb0377dbca7c170e815
JBenedik <jeremybenedik@gmail.com>
2019-02-07 19:05:04 -0600

    working on dojo survey

--- pythonStack/flask/flask_fundamentals/dojoSurvey/server.py
+++ pythonStack/flask/flask_fundamentals/dojoSurvey/server.py
@@ -7,7 +7,23 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def info_list():
-        print("Got Post Info")
+    print("*"*80)
+    print(request.form)
+    name_from_form = request.form['name']
+    color_from_form = request.form['favColor']
+    animal_from_form = request.form['favAnimal']
+    word_from_form = request.form['word']
+    print(word_from_form)
+    word_list = []
+    for i in request.form:
+        print(i)
+        if i == 'word':
+            word_list.append(request.form[i])
+    print(word_list)
+    comment_from_form = request.form['comments']
+    return render_template("result.html",name_on_template=name_from_form,color_on_template=color_from_form,animal_on_template=animal_from_form,word_on_template=word_from_form,comment_on_template=comment_from_form)
+
+
 
 
 if __name__=="__main__":
